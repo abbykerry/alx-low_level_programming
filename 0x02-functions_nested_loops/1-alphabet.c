2b685ba6cb6ca61a3901fa3ebd0ea6b7ba72bb3f
abbykerry <abbykerry17@gmail.com>
2022-09-14 15:15:51 -0700

    1. TASK

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,32 +1,19 @@
 #include “main.h”
-
 /**
-
  * print_alphabet – function to print abc
-
  *
-
  * Return: 0
-
  */
-
 Void print_alphabet(void)
-
 {
-
 	Char c;
 
-
-
 	For (c = ‘a’; c <= ‘z’; c++)
-
 	{
 
-		_putchar©;
-
+		_putchar(c);
 	}
 
 _putchar(‘\n’);
-
 }
 
