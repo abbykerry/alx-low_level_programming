15fc3c2004fb02b6b7849724d0bb590c3c0f97a5
abbykerry <abbykerry17@gmail.com>
2022-09-16 04:21:57 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#include "main.h"
+
+
+/**
+ * print_numbers - func that print all numbers from 0 to 9
+ *
+ * Return: returns 0-9 plus a new line
+ *
+ */
+
+void print_numbers(void)
+
+{
+	int j;
+
+	for (j = 0; j < 9; j++)
+		putchar(n);
+	putchar('\n');
+
+}
