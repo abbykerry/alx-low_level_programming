57b6f6dd50730accb3c7a5359ad462c75bc0d793
abbykerry <abbykerry17@gmail.com>
2022-09-22 09:41:30 -0700

    task 10 commit

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+
 int main(void)
 {
 	int n;
@@ -7,6 +8,7 @@ int main(void)
 	int a[5];
 
 	int *p;
+
 
 	a[2] = 1024;
 
@@ -29,10 +31,14 @@ int main(void)
 	 *
 	 */
 
+
 	*(p + 5) = 98;
+
 
 	/* ...so that this prints 98\n */
 
+
 	printf("a[2] = %d\n", a[2]);
+
 	return (0);
 }
